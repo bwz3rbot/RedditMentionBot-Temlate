cff50c38001d9160a06fa563cd44afe3e051252e
customwebtemplatedesigner <68409068+customwebtemplatedesigner@users.noreply.github.com>
2020-08-30 15:33:12 -0700

    Ready for templating

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+// Load with Environment Variables.
 const dotenv = require('dotenv').config({
     path: "./pw.env"
 })
+// Require the Stream Generator
 const inbox = require('./util/inboxStreamGenerator')
+
+// Run the messaging queue...
 inbox.streamUnreads();
+
+//It will continously run and when a new mention is received, it will add it the the queue to be processed when the ActionRequester object is ready for it.
+
+// Your code will be run from the BotService module each time a new mention is received.
--- src/service/BotService.js
+++ src/service/BotService.js
@@ -3,16 +3,32 @@ const Snoowrap = require('snoowrap')
 const requester = require('../config/snoo-config').actionRequester
 
 // This is where your code will go. It must return a promise!
+// This function is called each time a new message is popped from the array.
+// It gives you a pre-fetched 'RedditObject' of type mention.
 async function doSomething(item) {
+    // Checking if the item was saved will keep the bot from processing anything twice.
     if (!item.saved) {
         console.log(`processing item: ${item.id}`)
         console.dir(item)
 
+        //
+        // CODE STARTS HERE //
+
+
+
 
         await printContents(item)
         await replyToComment(item)
-        await saveItem(item)
 
+
+
+
+
+        // CODE ENDS HERE //
+        //
+
+        // Be sure to finish with saveItem(item) so the item will not be processed again.
+        await saveItem(item)
         return console.log(`item  ${item.id} successfully processed!`)
     } else {
         return console.log(`item ${item.id} saved already. Skipping...`)
@@ -23,7 +39,7 @@ async function doSomething(item) {
 }
 
 
-
+// Write your functions down here and call them inside of doSomething.
 
 const printContents = function (item) {
     console.log(('printing contents of item.body:\n ' + item.body).green)
